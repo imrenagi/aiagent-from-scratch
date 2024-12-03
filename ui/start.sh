932d90b72e9abee5e1d6b6fbfc32b2b96adf23b2
Imre Nagi <imre.nagi2812@gmail.com>
2024-12-03 02:30:37 +0100

    fix: enable tracing for reasoning engine
    Signed-off-by: Imre Nagi <imre.nagi2812@gmail.com>

--- ui/start.sh
+++ ui/start.sh
@@ -2,9 +2,9 @@
 
 pip install -r requirements.txt
 
-export UI_PROJECT_ID="pyconapac2024-testing"
+export UI_PROJECT_ID="imrenagi-devfest-2024"
 export UI_LOCATION="us-central1"  
-export UI_STAGING_BUCKET="gs://pyconapac24-staging-bucket-01"  
-export UI_REASONING_ENGINE_PATH="projects/423999378850/locations/us-central1/reasoningEngines/797013988742266880"
+export UI_STAGING_BUCKET="gs://devfest24-demo-bucket"  
+export UI_REASONING_ENGINE_PATH="projects/908311267620/locations/us-central1/reasoningEngines/7151100481752793088"
 
 streamlit run server.py --server.port=8080 --server.address=0.0.0.0
