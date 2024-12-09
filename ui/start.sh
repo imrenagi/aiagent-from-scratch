a0e6f8cfbade1523f7e15bcba0da8c9a445d954e
Imre Nagi <imre.nagi2812@gmail.com>
2024-12-09 23:46:16 +0100

    fix: use pg session history and deploy agent ui
    Signed-off-by: Imre Nagi <imre.nagi2812@gmail.com>

--- ui/start.sh
+++ ui/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 export UI_PROJECT_ID="imrenagi-devfest-2024"
 export UI_LOCATION="us-central1"  
 export UI_STAGING_BUCKET="gs://devfest24-demo-bucket"  
-export UI_REASONING_ENGINE_PATH="projects/908311267620/locations/us-central1/reasoningEngines/7151100481752793088"
+export UI_REASONING_ENGINE_PATH="projects/908311267620/locations/us-central1/reasoningEngines/5226374591005327360"
 
 streamlit run server.py --server.port=8080 --server.address=0.0.0.0
