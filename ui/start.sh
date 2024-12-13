f1ec03d391bae0a50916497b9e0db2c5673a5d6d
Imre Nagi <imre.nagi2812@gmail.com>
2024-12-13 01:39:38 +0100

    chore: finalize workshop
    Signed-off-by: Imre Nagi <imre.nagi2812@gmail.com>

--- ui/start.sh
+++ ui/start.sh
@@ -5,6 +5,6 @@
 export UI_PROJECT_ID="imrenagi-devfest-2024"
 export UI_LOCATION="us-central1"  
 export UI_STAGING_BUCKET="gs://devfest24-demo-bucket"  
-export UI_REASONING_ENGINE_PATH="projects/908311267620/locations/us-central1/reasoningEngines/5226374591005327360"
+export UI_REASONING_ENGINE_PATH="projects/908311267620/locations/us-central1/reasoningEngines/2387523529017917440"
 
 streamlit run server.py --server.port=8080 --server.address=0.0.0.0
